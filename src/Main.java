f5ade92d7b8191b6df8c8390ada6a1f55ee8412a
HoffMatt <MatthewHHoffmann@gmail.com>
2024-07-22 17:34:05 -0400

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         for( int i =0; i<10; i++){
             System.out.println("Matthew is great");
             Thread.sleep(2000);
