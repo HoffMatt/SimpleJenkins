384522e080abe66f0848a3607498b0117a4a41d7
HoffMatt <MatthewHHoffmann@gmail.com>
2024-07-22 17:33:27 -0400

    first commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
-
+        for( int i =0; i<10; i++){
+            System.out.println("Matthew is great");
+            Thread.sleep(2000);
+        }
     }
 }
